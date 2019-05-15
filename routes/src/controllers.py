2eb71985402d0767d53850fdbc07660904a36eb1
rsanchez-s <41480001+rsanchez-s@users.noreply.github.com>
2019-05-15 00:32:12 -0400

    Release (#44)
    * Update Jinja2 and added stage to dockerfile. (#26)
    
    * Devops 62 roberto 11 (#27)
    
    * Update Jinja2 and added stage to dockerfile.
    
    * Removed stages from Dockerfile 
    
    There currently exists a bug with Jenkins that prevents it from properly building an image from a Dockerfile with multiple stages.
    
    * Fne all satheesh sprint11 (#28)
    
    * page number changes
    
    * fixed an issue if attachment doesn't exist
    
    * fix FNE-758
    
    * F3X Image Generation changes
    
    * fixed f99 print
    
    * Added wkhtmltopdf to list of packages to install. (#30)
    
    * Fne all satheesh sprint12 (#29)
    
    * page number changes
    
    * fixed an issue if attachment doesn't exist
    
    * fix FNE-758
    
    * F3X Image Generation changes
    
    * fixed f99 print
    
    * F99 Html to PDF changes
    
    * F99 template changes (#31)
    
    * Fne 1015 roberto 12 (#32)
    
    * Added wkhtmltopdf to list of packages to install.
    
    * Added Beautifulsoup dependancy and changed to running the application as an unpriviledged user.
    
    * Fne 1015 roberto 12 (#33)
    
    * Added wkhtmltopdf to list of packages to install.
    
    * Added Beautifulsoup dependancy and changed to running the application as an unpriviledged user.
    
    * Added WeasyPrint 47 to list of dependancies
    
    * Fne all satheesh sprint12 v1 (#35)
    
    * F99 template changes
    
    * F99 cleanup
    
    * Fne 1015 roberto 12 (#34)
    
    * Added wkhtmltopdf to list of packages to install.
    
    * Added Beautifulsoup dependancy and changed to running the application as an unpriviledged user.
    
    * Added WeasyPrint 47 to list of dependancies
    
    * remove cairocffi from python environment.
    
    * Removed unused requirements
    
    * Fne 1015 roberto 12 (#36)
    
    * Added wkhtmltopdf to list of packages to install.
    
    * Added Beautifulsoup dependancy and changed to running the application as an unpriviledged user.
    
    * Added WeasyPrint 47 to list of dependancies
    
    * remove cairocffi from python environment.
    
    * Removed unused requirements
    
    * removed user 9000
    
    * removed user 9000 (#37)
    
    * Fne all satheesh sprint12 v1 (#38)
    
    * F99 template changes
    
    * F99 cleanup
    
    * fixed import
    
    * Importing bs4 (#39)
    
    * added nxvfd to docker image to run wkhtmltopdf headless. (#40)
    
    * Hotfix4 (#41)
    
    * added nxvfd to docker image to run wkhtmltopdf headless.
    
    * removed python optimization
    
    * Removed module testing (#42)
    
    * Hotfix6 (#43)
    
    * headless wkhtmltopdf
    
    * modified xvfb-run
    
    * added wkhtmltopdf to root directory
    
    * changed wkhtml to posix format
    
    * added xauth to list of dependecies

--- routes/src/controllers.py
+++ routes/src/controllers.py
@@ -3,7 +3,7 @@ import flask
 
 from flask import request
 from flask_cors import CORS
-from routes.src import tmoflask, form99
+from routes.src import tmoflask, form99, form3x
 
 logger = logging.getLogger()
 
@@ -29,6 +29,30 @@ def print_pdf():
     "success": "true"
     }
     """
-    form_type = request.form["form_type"]
-    if form_type == "F99":
-        return form99.print_f99_pdftk()
+    form_type = request.form['form_type']
+    if form_type == 'F99':
+        return form99.print_f99_pdftk_html('')
+    elif form_type == 'F3X':
+        return form3x.print_pdftk('')
+
+@app.route('/stamp_print', methods=['POST'])
+def stamp_print_pdf():
+    """
+    This function is being invoked from FECFile and Vendors
+    HTTP request needs to have form_type, file, and attachment_file
+    form_type : F99
+    json_file: please refer to below sample JSON
+    :return: return JSON response
+    sample:
+    {
+    "message": "",
+    "results": {
+        "pdf_url": "https://fecfile-dev-components.s3.amazonaws.com/output/bd78435a70a70d656145dae89e0e22bb.pdf"
+    },
+    "success": "true"
+    }
+    """
+    form_type = request.form['form_type']
+    if form_type == 'F99':
+        return form99.print_f99_pdftk_html('stamp')
+
