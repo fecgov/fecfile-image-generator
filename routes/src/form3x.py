e960ff83ecb6444890d6cb82b35c41431084cdeb
yeswanth kumar tella <yeswanth.tella@ctr.salientcrgt.com>
2020-02-20 14:38:14 -0500

    changes

--- routes/src/form3x.py
+++ routes/src/form3x.py
@@ -3088,6 +3088,12 @@ def build_payee_name_date_dict(index, key, sb_schedule_dict, sb_schedule_page_di
                                                                       + sb_schedule_dict['beneficiaryCandidateMiddleName'] + ','
                                                                       + sb_schedule_dict['beneficiaryCandidatePrefix'] + ','
                                                                       + sb_schedule_dict['beneficiaryCandidateSuffix'])
+        if key == 'electionCode':
+            if sb_schedule_dict[key][0] in ['P','G']:
+                sb_schedule_page_dict['electionType_' + str(index)] = sb_schedule_dict[key][0:1]
+            else:
+                sb_schedule_page_dict['electionType_' + str(index)] = 'O'
+            sb_schedule_page_dict['electionYear_' + str(index)] = sb_schedule_dict[key][1::]
 
         if key == 'expenditureDate':
             date_array = sb_schedule_dict[key].split("/")
